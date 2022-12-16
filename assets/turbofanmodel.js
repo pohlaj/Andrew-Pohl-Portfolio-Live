413a435e2e183f64cb7e7b428893095a7cb666df
pohlaj <pohlaj@clarkson.edu>
2022-12-16 08:54:15 -0500

    WIP 3D Model Page

--- assets/turbofanmodel.js
+++ assets/turbofanmodel.js
@@ -1,5 +1,5 @@
 
-import THREE from '../node_modules/three';
+import * as THREE from '../node_modules/three/src/Three.js';
 
 // Setup
 
@@ -8,13 +8,13 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#canvas'),
+  canvas: document.querySelector('#bg'),
 });
 
 renderer.setPixelRatio( window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
-camera.position.setX(-3);
+camera.position.setX(-3)
 
 renderer.render(scene, camera);
 
@@ -37,31 +37,30 @@ scene.add(pointLight, ambientLight);
 
 // Helpers
 
-//const lightHelper = new THREE.PointLightHelper(pointLight)
-//const gridHelper = new THREE.GridHelper(200, 50);
-//scene.add(lightHelper, gridHelper)
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper)
 
-const controls = new OrbitControls(camera, renderer.domElement);
+//const controls = new OrbitControls(camera, renderer.domElement);
 
-function addStar() {
-    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-    const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-    const star = new THREE.Mesh(geometry, material);
-  
-    const [x, y, z] = Array(3)
-      .fill()
-      .map(() => THREE.MathUtils.randFloatSpread(100));
-  
-    star.position.set(x, y, z);
-    scene.add(star);
-  }
-  
-Array(200).fill().forEach(addStar);
+
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load('assets\images\IMG_2662.png');
+const spaceTexture = new THREE.TextureLoader().load('../assets/images/20210911-IMG_8093.jpg');
 scene.background = spaceTexture;
+
+// Ground
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry( 10, 10 ),
+  new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } )
+);
+plane.rotation.x = - Math.PI / 2;
+plane.position.y = - 1;
+scene.add( plane );
+
+plane.receiveShadow = true;
 
 // Scroll Animation
 
@@ -81,9 +80,9 @@ moveCamera();
 function animate() {
     requestAnimationFrame(animate);
 
-    torus.rotation.x += 0.01;
+    torus.rotation.x += 0.005;
     torus.rotation.y += 0.005;
-    torus.rotation.z += 0.01;
+    torus.rotation.z += 0.0;
 
     // controls.update();
 
